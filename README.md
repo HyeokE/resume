8b0eff459882d51553bf6bb18c134507d2d73b11
HyeokE <jhjeong00@gmail.com>
2023-07-08 13:48:37 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# 이력서
-
-[이현섭님의 이력서 코드](https://github.com/HyunSeob/resume)를 사용하여 제작하였습니다.
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Resume from './Resume';
-
-function App() {
-  return <Resume />;
-}
-
-export default App;
